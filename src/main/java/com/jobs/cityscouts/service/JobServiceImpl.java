468e549f54c5cb06f1c8e4196e6a14887e7550d8
Ian Dancan Mongare <dancanian25@gmail.com>
2024-02-22 17:28:29 +0300

    revamped the serviceImpl class

--- src/main/java/com/jobs/cityscouts/service/JobServiceImpl.java
+++ src/main/java/com/jobs/cityscouts/service/JobServiceImpl.java
@@ -4,6 +4,7 @@ import com.jobs.cityscouts.entity.jobEntity.Job;
 import com.jobs.cityscouts.exception.JobNotFoundException;
 import com.jobs.cityscouts.repository.JobRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataAccessException;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDateTime;
@@ -34,37 +35,41 @@ public class JobServiceImpl implements JobService{
 
     @Override
     public boolean updateJob(Long id, Job updatedJob) {
-        List<Job> jobs = findAllJobs();
-        //iterate
-        for(Job job: jobs){
+        Optional<Job> optionalJob = jobRepository.findById(id);
+        try {
             //check if the id matches
-            if (job.getId().equals(id)){
+            if (optionalJob.isPresent()) {
                 //set new values
+                Job job = optionalJob.get();
                 job.setTitle(updatedJob.getTitle());
                 job.setDescription(updatedJob.getDescription());
                 job.setMaxSalary(updatedJob.getMaxSalary());
                 job.setMinSalary(updatedJob.getMinSalary());
                 job.setLocation(updatedJob.getLocation());
                 updatedJob.setEditedAt(LocalDateTime.now());
+
+                jobRepository.save(updatedJob);
                 return true;
+
             }
+        }catch (DataAccessException e){
+            e.printStackTrace();
         }
         return false;
     }
 
     @Override
     public boolean deleteJob(Long id) {
-        List<Job> jobs = findAllJobs();
-        //check if it exists
-        Iterator<Job> iterator = jobs.iterator();
-        while(iterator.hasNext()){
-            Job job = iterator.next();
-            //check if id matches
-            if (job.getId().equals(id)){
-                iterator.remove();
+        try {
+            if (jobRepository.existsById(id)) { // Check if the job exists before attempting to delete
+                jobRepository.deleteById(id); // Directly delete the job by ID
+
                 return true;
             }
+        }catch (Exception e){
+            e.printStackTrace();
         }
+
         return false;
     }
 }
